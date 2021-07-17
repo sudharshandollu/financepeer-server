2b04a1dacf35a08d40dae2e85c3f134bd819714d
sudharshandollu <sudharshandollu03@gmail.com>
2021-07-17 07:13:23 +0000

    changed port

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ const initializeDbAndServer = async () => {
       driver: sqlite3.Database,
     });
 
-    app.listen(80, () =>
+    app.listen(3000, () =>
       console.log("Server Running at http://localhost:5000/")
     );
   } catch (error) {
