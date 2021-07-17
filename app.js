77c6e2f7c89e7f1f7da2cbff09e79cb4719ce58d
sudharshandollu <sudharshandollu03@gmail.com>
2021-07-17 06:54:32 +0000

    remove credentials

--- app.js
+++ app.js
@@ -19,8 +19,19 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({ credentials: true }));
+app.use(cors());
 app.use(cookieParser());
+
+/*app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+}); */
 
 let database = null;
 
